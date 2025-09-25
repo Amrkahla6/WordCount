1b60fca4f491f84bf6239c26e806e2fe324d82a9
Amr Kahla <amrkahla6@gmail.com>
2025-09-25 20:33:18 +0300

    Handle Not Found Exception

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,29 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws Exception {
-        File file = new File("pg98.txt");
-        Scanner scanner = new Scanner(file);
 
-        int word = 0;
+        try{
+            File file = new File("pg98.txt");
+            Scanner scanner = new Scanner(file);
 
-        while (scanner.hasNextLine()){
-            String line = scanner.nextLine();
-            word += line.split(" ").length;
+            int word = 0;
+
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                word += line.split(" ").length;
+            }
+
+            System.out.println("The file content "+word+" words");
+        } catch(FileNotFoundException exception) {
+            // Handle the situation by letting the user know what happened
+            System.out.println("Cannot find that file");
         }
 
-        System.out.println("The file content "+word+" words");
+
     }
 }
